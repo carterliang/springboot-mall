6ea805e4134550a5fbba411cae9dfa477221f812
carterliang <carterliangapt@gmail.com>
2022-07-29 16:50:34 +0800

    商品功能Springboot 實作刪除商品

--- demo/src/main/java/com/springmall/demo/controller/ProductController.java
+++ demo/src/main/java/com/springmall/demo/controller/ProductController.java
@@ -46,8 +46,8 @@ public class ProductController {
     }
 
 
-    @DeleteMapping("/prudcuts/{productId}")
-    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId){
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProductById(@PathVariable Integer productId){
         productService.deleteProductById(productId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
